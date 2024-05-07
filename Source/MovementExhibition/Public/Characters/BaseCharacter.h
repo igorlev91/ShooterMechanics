81f0ace63c9e33b047de6e95011576a3a85afeab
Igor Levdansky <igorlevdansky91@gmail.com>
2024-05-07 13:53:06 +0300

    Health and shield component
    Added health and shield component.
    Created shooter character child of base character.

---
+++ Source/MovementExhibition/Public/Characters/BaseCharacter.h
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UHealthComponent;
+
+UCLASS()
+class MOVEMENTEXHIBITION_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void UpdateSprintStatus() const;
+
+	void OnDeath();
+	
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
+	void RequestMove(const FVector2d& AxisValue);
+	void RequestLook(const FVector2d& AxisValue);
+	void RequestToggleSprint() const;
+	void RequestJump();
+	void RequestStopJumping();
+
+	UFUNCTION(BlueprintPure)
+	float GetMaxHealth() const;
+
+	UFUNCTION(BlueprintPure)
+	float GetCurrentHealth() const;
+	
+// Components
+protected:
+	UPROPERTY(VisibleAnywhere, NoClear)
+	TObjectPtr<USpringArmComponent> CameraBoom;
+	
+	UPROPERTY(VisibleAnywhere, NoClear)
+	TObjectPtr<UCameraComponent> CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, NoClear)
+	TObjectPtr<UHealthComponent> HealthComponent;
+
+// Properties
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputMappingContext> MappingContext;
+
+	UPROPERTY(EditAnywhere, Category="Look", meta=(ClampMin="0", UIMin="0", ForceUnits="deg/s"))
+	float BaseLookUpRate = 90.f;
+
+	UPROPERTY(EditAnywhere, Category="Look", meta=(ClampMin="0", UIMin="0", ForceUnits="deg/s"))
+	float BaseLookRightRate = 90.f;
+
+	UPROPERTY(EditAnywhere, Category="Sprint", meta=(ClampMin="0", UIMin="0", ForceUnits="cm/s"))
+	float SprintSpeed = 900.f;
+
+	UPROPERTY(Transient)
+	float WalkSpeed = 600.f;
+};
